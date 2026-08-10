3c7dd5d33093ce98a56846b78f29a56de86d596c
dlclgjs <lechihun67@gmail.com>
2026-08-11 02:38:39 +0900

    '모빌리티 정산 서비스'의 '원장(Ledger) 관리 시스템' 레포지토리입니다!

---
+++ src/main/java/com/example/easyadj/EasyadjApplication.java
@@ -0,0 +1,13 @@
+package com.example.easyadj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyadjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyadjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/easyadj/EasyadjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easyadj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyadjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
